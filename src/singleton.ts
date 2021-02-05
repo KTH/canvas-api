2899db2ee70f91858b9fbf1dbc18555b7d6fb651
Carlos Saito <carlos@saito.style>
2021-02-05 09:42:25 +0100

    Singleton version

---
+++ src/singleton.ts
@@ -0,0 +1,10 @@
+import { ExtendOptions } from "got";
+import CanvasAPI from "./index";
+
+let singleton: CanvasAPI | null = null;
+
+export function init (apiUrl: string, apiToken: string, options: ExtendOptions) {
+  singleton = new CanvasAPI(apiUrl, apiToken, options)
+}
+
+export default singleton;

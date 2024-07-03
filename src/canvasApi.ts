59eb56589e23f7c9f789e87dcc7ad8914e129fc9
Carlos Saito <carlos@saito.style>
2024-07-03 11:12:45 +0200

    First implementation of sisImport

--- src/canvasApi.ts
+++ src/canvasApi.ts
@@ -1,1 +1,39 @@
-export class CanvasApi {}
+import { request, FormData } from "undici";
+import { readFile } from "node:fs/promises";
+
+export type CanvasApiResponse = {
+  statusCode: number;
+  headers: Map<string, string | string[]>;
+  body: unknown;
+};
+
+export class CanvasApi {
+  apiUrl: URL;
+  token: string;
+
+  constructor(apiUrl: string, token: string) {
+    this.apiUrl = new URL(apiUrl);
+    this.token = token;
+  }
+
+  async sisImport(attachment: string) {
+    const file = await readFile(attachment).then(
+      (buffer) => new Blob([buffer])
+    );
+
+    const formData = new FormData();
+    formData.set("attachment", file);
+
+    const endpoint = new URL("accounts/1/sis_import", this.apiUrl);
+    const response = await request(endpoint, {
+      method: "POST",
+      body: formData,
+    });
+
+    return {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      body: response.body.json(),
+    };
+  }
+}

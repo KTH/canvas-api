52d65e117f580a032ee2380d59dd69f7ecafa802
Carlos Saito <carlos@saito.style>
2024-07-03 16:00:06 +0200

    Read dotenv from jest

--- integration-tests/simple-get.test.ts
+++ integration-tests/simple-get.test.ts
@@ -3,10 +3,7 @@ import { CanvasApi } from "@kth/canvas-api";
 
 describe("Simple requests", () => {
   it("Should fail if no token is given", async () => {
-    const client = new CanvasApi(
-      "https://kth.test.instructure.com/api/v1/",
-      ""
-    );
+    const client = new CanvasApi(process.env.CANVAS_API_URL!, "");
 
     expect(() => client.get("accounts/1")).rejects.toThrow();
     // await client.get("accounts/1").catch((err) => );
--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
   testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },

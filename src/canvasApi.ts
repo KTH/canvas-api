542a6a4a2a3bcffbdded5d50e1a5139356e418e1
Carlos Saito <carlos@saito.style>
2024-07-03 13:20:36 +0200

    Normalize requests

--- src/canvasApi.ts
+++ src/canvasApi.ts
@@ -1,11 +1,51 @@
 import { request, FormData } from "undici";
+import type { Dispatcher } from "undici";
 import { readFile } from "node:fs/promises";
+import {
+  CanvasApiRequestError,
+  CanvasApiResponseError,
+} from "./canvasApiError";
+
+export type CanvasApiResponseBody =
+  | { json: null; text: string }
+  | { json: unknown; text: null };
 
 export type CanvasApiResponse = {
   statusCode: number;
-  headers: Map<string, string | string[]>;
-  body: unknown;
-};
+  headers: Record<string, string | string[] | undefined>;
+} & CanvasApiResponseBody;
+
+async function handleRequest(
+  responsePromise: Promise<Dispatcher.ResponseData>
+): Promise<CanvasApiResponse> {
+  const response = await responsePromise.catch(() => {
+    throw new CanvasApiRequestError("");
+  });
+
+  if (response.statusCode >= 300) {
+    throw new CanvasApiResponseError(response);
+  }
+
+  const text = await response.body.text();
+
+  try {
+    const json = JSON.parse(text);
+
+    return {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      json,
+      text: null,
+    };
+  } catch (e) {
+    return {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      json: null,
+      text,
+    };
+  }
+}
 
 export class CanvasApi {
   apiUrl: URL;
@@ -16,7 +56,7 @@ export class CanvasApi {
     this.token = token;
   }
 
-  async sisImport(attachment: string) {
+  async sisImport(attachment: string): Promise<CanvasApiResponse> {
     const file = await readFile(attachment).then(
       (buffer) => new Blob([buffer])
     );
@@ -25,15 +65,11 @@ export class CanvasApi {
     formData.set("attachment", file);
 
     const endpoint = new URL("accounts/1/sis_import", this.apiUrl);
-    const response = await request(endpoint, {
-      method: "POST",
-      body: formData,
-    });
-
-    return {
-      statusCode: response.statusCode,
-      headers: response.headers,
-      body: response.body.json(),
-    };
+    return handleRequest(
+      request(endpoint, {
+        method: "POST",
+        body: formData,
+      })
+    );
   }
 }
--- src/canvasApiError.ts
+++ src/canvasApiError.ts
@@ -1,1 +1,24 @@
-export class CanvasApiError {}
+import { Dispatcher } from "undici";
+/** Super-class for CanvasApi library */
+export class CanvasApiError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "CanvasApiError";
+  }
+}
+
+/**
+ * Thrown when Canvas has returned a non-200 response
+ */
+export class CanvasApiResponseError extends CanvasApiError {
+  // TODO: spec extra fields
+  constructor(response: Dispatcher.ResponseData) {
+    super("Canvas API response error");
+
+    // TODO: check response data
+  }
+}
+
+export class CanvasApiRequestError extends CanvasApiError {
+  // TODO
+}

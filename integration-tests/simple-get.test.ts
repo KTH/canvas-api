311f77fb7be96195b79e1acd8b1067f6af92911a
Carlos Saito <carlos@saito.style>
2024-07-04 13:41:51 +0200

    Add test with query parameters

--- integration-tests/simple-get.test.ts
+++ integration-tests/simple-get.test.ts
@@ -8,7 +8,13 @@ const accountSchema = z.object({
   workflow_state: z.string(),
 });
 
-describe("Simple requests", () => {
+const enrollmentsSchema = z.array(
+  z.object({
+    role_id: z.number(),
+  })
+);
+
+describe("GET requests without parameters", () => {
   it("Should fail if no token is given", async () => {
     const client = new CanvasApi(process.env.CANVAS_API_URL!, "");
 
@@ -32,5 +38,24 @@ describe("Simple requests", () => {
         "workflow_state": "active",
       }
     `);
+  });
+});
+
+describe("GET requests with query parameters", () => {
+  it("should return right results", async () => {
+    const client = new CanvasApi(
+      process.env.CANVAS_API_URL!,
+      process.env.CANVAS_API_TOKEN!
+    );
+
+    const enrollments = await client
+      .get("courses/1/enrollments", { role_id: [3, 6], per_page: 50 })
+      .then((r) => enrollmentsSchema.parse(r.json));
+
+    const uniqueRoles = new Set(enrollments.map((e) => e.role_id));
+
+    expect(uniqueRoles.size).toBe(2);
+    expect(uniqueRoles.has(3)).toBeTruthy();
+    expect(uniqueRoles.has(6)).toBeTruthy();
   });
 });

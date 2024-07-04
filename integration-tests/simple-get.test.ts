8ad07fd01524617ab6dd0c2cd78b63a75645407f
Carlos Saito <carlos@saito.style>
2024-07-04 10:21:34 +0200

    Update test
    Use zod for parsing

--- integration-tests/simple-get.test.ts
+++ integration-tests/simple-get.test.ts
@@ -1,5 +1,12 @@
 /** Make simple requests (GET, POST, PUT, etc.) to actual Canvas */
 import { CanvasApi, CanvasApiResponseError } from "@kth/canvas-api";
+import { z } from "zod";
+
+const accountSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  workflow_state: z.string(),
+});
 
 describe("Simple requests", () => {
   it("Should fail if no token is given", async () => {
@@ -16,24 +23,13 @@ describe("Simple requests", () => {
       process.env.CANVAS_API_TOKEN!
     );
     const { json, statusCode, text } = await client.get("accounts/1");
-
-    expect({ json, statusCode, text }).toMatchInlineSnapshot(`
+    expect(statusCode).toBe(200);
+    expect(text).toBeNull();
+    expect(accountSchema.parse(json)).toMatchInlineSnapshot(`
       {
-        "json": {
-          "course_template_id": null,
-          "default_group_storage_quota_mb": 1000,
-          "default_storage_quota_mb": 2000,
-          "default_time_zone": "Europe/Stockholm",
-          "default_user_storage_quota_mb": 1000,
-          "id": 1,
-          "name": "KTH Royal Institute of Technology",
-          "parent_account_id": null,
-          "root_account_id": null,
-          "uuid": "ySt5cF5tiEU8j5oIzxT2J98caTu54Vl6y9s6gYdS",
-          "workflow_state": "active",
-        },
-        "statusCode": 200,
-        "text": null,
+        "id": 1,
+        "name": "KTH Royal Institute of Technology",
+        "workflow_state": "active",
       }
     `);
   });

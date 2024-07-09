944ec1d83212401a01cfc93fb8899f26b1842d45
Carlos Saito <carlos@saito.style>
2024-07-09 10:12:59 +0200

    Structure tests

--- src/canvasApi.test.ts
+++ src/canvasApi.test.ts
@@ -8,37 +8,37 @@ import {
 import { createServer } from "node:http";
 
 describe("queryParameters", () => {
-  test("should parse primitives parameters correctly", () => {
+  it("should parse primitives parameters correctly", () => {
     const input = { page: 1, id: "2" };
     const expected = "?page=1&id=2";
     expect(stringifyQueryParameters(input)).toEqual(expected);
   });
 
-  test("should work with empty objects", () => {
+  it("should work with empty objects", () => {
     const input = {};
     const expected = "";
     expect(stringifyQueryParameters(input)).toEqual(expected);
   });
 
-  test("should work with one parameter", () => {
+  it("should work with one parameter", () => {
     const input = { id: 3 };
     const expected = "?id=3";
     expect(stringifyQueryParameters(input)).toEqual(expected);
   });
 
-  test("should parse array parameters correctly", () => {
+  it("should parse array parameters correctly", () => {
     const input = { role: [3, 10] };
     const expected = "?role[]=3&role[]=10";
     expect(stringifyQueryParameters(input)).toEqual(expected);
   });
 
-  test("should work with arrays mixing numbers and strings", () => {
+  it("should work with arrays mixing numbers and strings", () => {
     const input = { role: [3, "10"] };
     const expected = "?role[]=3&role[]=10";
     expect(stringifyQueryParameters(input)).toEqual(expected);
   });
 
-  test("emtpy arrays should be ignored", () => {
+  it("emtpy arrays should be ignored", () => {
     const input = { role: [] };
     const expected = "";
     expect(stringifyQueryParameters(input)).toEqual(expected);
@@ -99,21 +99,21 @@ describe("listItems", () => {
     setGlobalDispatcher(defaultAgent);
   });
 
-  test("returns correct iterable", async () => {
+  it("returns correct iterable", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const output = await canvas.listItems("courses/page1").toArray();
 
     expect(output).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test("ignores link headers that are not 'rel=next'", async () => {
+  it("ignores link headers that are not 'rel=next'", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const output = await canvas.listItems("rare-header").toArray();
 
     expect(output).toEqual([1]);
   });
 
-  test("uses the URL in header in an opaque way", async () => {
+  it("uses the URL in header in an opaque way", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const output = await canvas
       .listItems("assignments/page1", { arg: 1 })
@@ -138,7 +138,7 @@ describe("`get` can parse JSON response", () => {
       .reply(200, "This is not a { json");
   });
 
-  test("Parsing a valid json", async () => {
+  it("Parsing a valid json", async () => {
     const canvas = new CanvasApi("https://canvas.kth.se/", "");
     const { json, text } = await canvas.get("example");
 
@@ -152,7 +152,7 @@ describe("`get` can parse JSON response", () => {
     `);
   });
 
-  test("Parsing an invalid json", async () => {
+  it("Parsing an invalid json", async () => {
     const canvas = new CanvasApi("https://canvas.kth.se/", "");
     const { json, text } = await canvas.get("example-text");
 
@@ -185,7 +185,7 @@ describe("CanvasApiResponseError", () => {
       .reply(418, "I am a teapot and invalid JSON )");
   });
 
-  test("errored GET request", async () => {
+  it("errored GET request", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const error = await canvas.get("errored").catch((e) => e);
 
@@ -203,7 +203,7 @@ describe("CanvasApiResponseError", () => {
     `);
   });
 
-  test("errored POST request with valid JSON", async () => {
+  it("errored POST request with valid JSON", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const error = await canvas.request("errored", "POST").catch((e) => e);
 
@@ -221,7 +221,7 @@ describe("CanvasApiResponseError", () => {
     `);
   });
 
-  test("errored PUT request with invalid JSON", async () => {
+  it("errored PUT request with invalid JSON", async () => {
     const canvas = new CanvasApi("https://canvas.local/", "");
     const error = await canvas.request("errored", "PUT").catch((e) => e);
 
@@ -257,7 +257,7 @@ describe("method-level timeout", () => {
     server.close();
   });
 
-  test("times out before the response", async () => {
+  it("times out before the response", async () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const port = (server.address() as any).port;
     const canvas = new CanvasApi(`http://0.0.0.0:${port}/`, "");
@@ -272,7 +272,7 @@ describe("method-level timeout", () => {
     expect(t2 - t1).toBeLessThan(120);
   });
 
-  test("overrides globally set timeout", async () => {
+  it("overrides globally set timeout", async () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const port = (server.address() as any).port;
     const canvas = new CanvasApi(`http://0.0.0.0:${port}/`, "", {
--- src/extendedGenerator.test.ts
+++ src/extendedGenerator.test.ts
@@ -1,42 +1,46 @@
 import { ExtendedGenerator } from "./extendedGenerator";
 
-test("extendGenerator returns a valid generator", async () => {
-  async function* gen() {
-    yield 1;
-  }
+describe("constructor", () => {
+  it("implements the iterator protocol", async () => {
+    async function* gen() {
+      yield 1;
+    }
 
-  const g2 = new ExtendedGenerator(gen());
+    const g2 = new ExtendedGenerator(gen());
 
-  for await (const v of g2) {
-    expect(v).toBe(1);
-  }
+    for await (const v of g2) {
+      expect(v).toBe(1);
+    }
+  });
 });
 
-test("AugmentedIterator.toArray works without arguments", async () => {
-  async function* gen() {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-  const gen2 = new ExtendedGenerator(gen());
+describe(".toArray()", () => {
+  it("works without arguments", async () => {
+    async function* gen() {
+      yield 1;
+      yield 2;
+      yield 3;
+    }
+    const gen2 = new ExtendedGenerator(gen());
 
-  await expect(gen2.toArray()).resolves.toEqual([1, 2, 3]);
-});
+    await expect(gen2.toArray()).resolves.toEqual([1, 2, 3]);
+  });
 
-test("AugmentedIterator.toArray does not restart the iteration", async () => {
-  async function* gen() {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-  const gen2 = new ExtendedGenerator(gen());
+  it("does not restart the iteration", async () => {
+    async function* gen() {
+      yield 1;
+      yield 2;
+      yield 3;
+    }
+    const gen2 = new ExtendedGenerator(gen());
 
-  await gen2.next();
-  await expect(gen2.toArray()).resolves.toEqual([2, 3]);
+    await gen2.next();
+    await expect(gen2.toArray()).resolves.toEqual([2, 3]);
+  });
 });
 
 describe("lazy behavior (does not call generator if not needed)", () => {
-  test("with simple `take(0)`", async () => {
+  it("with simple `take(0)`", async () => {
     // eslint-disable-next-line require-yield
     async function* gen() {
       throw new Error();
@@ -47,7 +51,7 @@ describe("lazy behavior (does not call generator if not needed)", () => {
     expect(await gen2.take(0).toArray()).toEqual([]);
   });
 
-  test("with filter and take", async () => {
+  it("with filter and take", async () => {
     // eslint-disable-next-line require-yield
     async function* gen() {
       throw new Error();
